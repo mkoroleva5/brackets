a829ee7d32baeec2039af0000e0b4b2d3a0d2454
Koroleva Maria <mkoroleva5@gmail.com>
2022-07-26 18:43:08 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   let arr = [];
   const openBrackets = ['(', '{', '[', '|', '1', '3', '5', '7', '8']
+  const brackectsPair = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['|']: '|',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8',
+  }
+
   for (let i = 0; i < str.length; i++) {
-    if (openBrackets.includes(str[i])) {
-      arr.push(str[i]);
+
+    let cur = str[i];
+    if (openBrackets.includes(cur)) {
+      arr.push(cur);
     } else {
       if (arr.length === 0) {
         return false;
       }
 
-      if (bracketsConfig[str[i]] === arr[arr.length-1]) {
+      let top = arr[arr.length - 1];
+      if (brackectsPair[cur] === top) {
         arr.pop();
       } else {
         return false;
