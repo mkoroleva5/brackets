6303128fc8ec901fef64f36d832e32b36c32ec04
Koroleva Maria <mkoroleva5@gmail.com>
2022-07-26 18:30:03 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  const openBrackets = ['(', '{', '[', '|', '1', '3', '5', '7', '8']
+  for (let i = 0; i < str.length; i++) {
+    if (openBrackets.includes(str[i])) {
+      arr.push(str[i]);
+    } else {
+      if (arr.length === 0) {
+        return false;
+      }
+
+      if (bracketsConfig[str[i]] === arr[arr.length-1]) {
+        arr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return arr.length === 0;
 }
