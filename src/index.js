76c0560880f4eafc29b1fdb6675a5bb19991b7c8
Koroleva Maria <mkoroleva5@gmail.com>
2022-08-01 17:58:40 +0300

    fix: add solution for equal brackets

--- src/index.js
+++ src/index.js
@@ -11,21 +11,36 @@ module.exports = function check(str, bracketsConfig) {
     ['6']: '5',
     ['7']: '7',
     ['8']: '8',
-  }
+  };
+  const equalBrackets = [];
+
+  bracketsConfig.forEach(el => {
+    if(el[0] === el[1]) {
+      equalBrackets.push(el[0])
+    }
+  });
 
   for (let i = 0; i < str.length; i++) {
 
     let cur = str[i];
-    if (openBrackets.includes(cur)) {
+    let top = arr[arr.length - 1];
+
+    if (arr.includes(cur) && equalBrackets.includes(cur)) {
+      if (cur = top) {
+        arr.pop(cur);
+      } else {
+        return false;
+      }
+    } else if (openBrackets.includes(cur)) {
       arr.push(cur);
     } else {
       if (arr.length === 0) {
         return false;
       }
 
-      let top = arr[arr.length - 1];
+      
       if (brackectsPair[cur] === top) {
-        arr.pop();
+        arr.pop(cur);
       } else {
         return false;
       }
